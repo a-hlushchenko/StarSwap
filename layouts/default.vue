532d4e1cb4a8213390cb587fe1fc9186f4568897
a-hlushchenko <glantoshka@gmail.com>
2025-01-07 00:59:43 +0200

    change footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,7 +27,7 @@ const { isSettings, isWallet } = storeToRefs(useSettingsStore());
 
 <style scoped>
 .main {
-  padding: calc(--fullscreen + --header + 1rem) 0;
+  padding: calc(var(--fullscreen) + var(--header) + 1rem) 0;
   overflow: auto;
   height: 100%;
 }
